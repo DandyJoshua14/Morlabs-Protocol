4ad4d112c4802def01546677a01fea6af5d50deb
DandyJoshua14 <dandyjoshua14@gmail.com>
2024-12-26 13:10:24 +0100

    seo 2

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,19 +5,19 @@ export default function sitemap(): MetadataRoute.Sitemap {
         {
             url: 'https://morlabsprotocol.com',
             lastModified: new Date(),
-            changeFrequency: 'yearly',
+
             priority: 1,
         },
         {
             url: 'https://morlabsprotocol.com/about',
             lastModified: new Date(),
-            changeFrequency: 'monthly',
+
             priority: 0.8,
         },
         {
             url: 'https://morlabsprotocol.com/blog',
             lastModified: new Date(),
-            changeFrequency: 'weekly',
+
             priority: 0.5,
         },
     ]
