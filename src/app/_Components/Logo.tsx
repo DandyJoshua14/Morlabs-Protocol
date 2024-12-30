b067bde71f58a7322a39c77a774210e789235dbd
alivexem <athkinstestimony@gmail.com>
2024-12-30 11:42:29 +0100

    Added black background

--- src/app/_Components/Logo.tsx
+++ src/app/_Components/Logo.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const Logo = () => {
   return (
     <div className='left-10 flex items-center'>
-      <Image src="/morlabs-logo.png" alt="Morlabs Protocol" width={150} height={30} className='object-contain' />
+      <Image src="/morlabs-logo.png" alt="Morlabs Protocol" width={150} height={30} className='object-contain md:w-[150px] md:h-[30px]' />
     </div>
   )
 }
