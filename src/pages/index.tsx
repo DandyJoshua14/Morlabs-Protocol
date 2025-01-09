21c5e2069a4846ff013f14faba934d31f1d0d628
genzybas <genzybas@gmail.com>
2025-01-09 23:42:51 +0100

    Landing page touch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import { FixedPlugin } from "@/components";
 import { FaNetworkWired, FaLock, FaUserShield, FaFileContract, FaRocket, FaListAlt, FaSearch, FaFlask } from "react-icons/fa";
 import { FaEthereum, FaBitcoin } from 'react-icons/fa';
 import { SiSolana, SiPolkadot, SiChainlink } from 'react-icons/si';
-import { MagnifyingGlassIcon, ArrowPathIcon, CloudIcon } from "@heroicons/react/24/outline";
+import { MagnifyingGlassIcon, ArrowPathIcon, CloudArrowUpIcon } from "@heroicons/react/24/outline";
 
 // sections
 import { default as Hero } from "./hero";
@@ -12,6 +12,7 @@ import { default as Hero } from "./hero";
 import VideoIntro from "./get-started";
 import Feature from "./feature";
 import Testimonials from "./testimonials";
+import FAQ from "./faq";
 // import Faqs from "./faqs";
 
 // export default function Campaign() {
@@ -57,7 +58,7 @@ export default function Home() {
               {/* <div className="flex flex-col justify-center lg:mt-10 p-[1px] items-center bg-gradient-to-l from-green-500 to-black rounded-2xl"> */}
 
               <div className="flex flex-col justify-center mt-[-7px] py-24 items-center bg-gradient-to-l from-green-700 to-black rounded-2xl px-14 lg:mb-12">
-                  <span><CloudIcon className="h-[100px] w-[100px] text-white" /></span>
+                  <span><CloudArrowUpIcon className="h-[100px] w-[100px] text-white" /></span>
                   <span className="text-lg text-center text-white">Go live with web3 functionality</span>
                 {/* </div> */}
               </div>
@@ -71,6 +72,7 @@ export default function Home() {
         <APISection />
         <VideoIntro />
         <Testimonials />
+        <FAQ />
         <Footer />
         {/* <FixedPlugin /> */}
       </div>
