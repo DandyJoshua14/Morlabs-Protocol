fa893aa02715ae121ffc79097b560c2cd28eed26
genzybas <genzybas@gmail.com>
2025-01-09 20:35:28 +0100

    landing page sketch2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { FixedPlugin } from "@/components";
 import { FaNetworkWired, FaLock, FaUserShield, FaFileContract, FaRocket, FaListAlt, FaSearch, FaFlask } from "react-icons/fa";
 import { FaEthereum, FaBitcoin } from 'react-icons/fa';
 import { SiSolana, SiPolkadot, SiChainlink } from 'react-icons/si';
+import { MagnifyingGlassIcon, ArrowPathIcon, CloudIcon } from "@heroicons/react/24/outline";
 
 // sections
 import { default as Hero } from "./hero";
@@ -34,31 +35,31 @@ export default function Home() {
 
 
         <div className="flex flex-col items-center py-10  px-2">
-          <h2 className="text-4xl lg:text-5xl font-bold mb-6 text-center leading-relaxed w-[90%] lg:w-[50%] text-white py-5">Simplifying the integration of APIs into Web3 projects</h2>
+          <h2 className="text-4xl lg:text-5xl font-bold mb-6 text-center leading-relaxed w-[90%] lg:w-[50%] text-white py-5">How it Works</h2>
           {/* <div className="grid grid-cols-1 md:grid-cols-5 gap-8"> */}
 
           <div className="flex justify-around w-full items-center">
             <div className="flex flex-col lg:flex-row justify-center items-center md:gap-x-4 gap-y-8">
 
-              <div className="flex flex-col justify-center mt-10 p-[1px] items-center bg-gradient-to-t from-green-500 to-purple-500 rounded-2xl">
+              {/* <div className="flex flex-col justify-center mt-10 p-[1px] items-center bg-gradient-to-t from-green-500 to-purple-500 rounded-2xl"> */}
 
-                <div style={{ background: '#0C0D0C' }} className="flex flex-col items-center rounded-2xl border-gradient py-14 px-24">
-                  <span className="text-5xl font-bold" style={{ color: '#41FEB5' }}>1000+</span>
-                  <span className="text-lg" style={{ color: '#41FEB5' }}>users per month</span>
-                </div>
+              <div className="flex flex-col justify-center mt-[-7px] py-24 items-center bg-gradient-to-l from-green-700 to-black rounded-2xl px-14 lg:mb-12">
+                  <span><MagnifyingGlassIcon className="h-[100px] w-[100px] text-white pb-5" /></span>
+                  <span className="text-lg text-white">Browse SDKs & APIs</span>
+                {/* </div> */}
               </div>
-              <div className="flex flex-col justify-center mt-[-7px] py-10 items-center bg-gradient-to-t from-green-500 to-purple-500 rounded-2xl px-8 lg:mb-12">
-                <span className="text-5xl font-bold text-white">92%</span>
-                <span className="text-lg text-gray-200 w-[60%] text-center">
-                  of users are satisfied with the platform
+              <div className="flex flex-col justify-center mt-[-7px] py-24 items-center bg-gradient-to-l from-green-700 to-black rounded-2xl px-14 lg:mb-12">
+                <span><ArrowPathIcon className="h-[90px] w-[90px] text-white" /></span>
+                <span className="text-lg text-white w-[60%] text-center">
+                Integrate seamlessly into your project
                 </span>
               </div>
-              <div className="flex flex-col justify-center lg:mt-10 p-[1px] items-center bg-gradient-to-t from-green-500 to-purple-500 rounded-2xl">
+              {/* <div className="flex flex-col justify-center lg:mt-10 p-[1px] items-center bg-gradient-to-l from-green-500 to-black rounded-2xl"> */}
 
-                <div style={{ background: '#0C0D0C' }} className="flex flex-col items-center rounded-2xl border-gradient py-14 px-24">
-                  <span className="text-5xl font-bold" style={{ color: '#41FEB5' }}>5000+</span>
-                  <span className="text-lg text-center" style={{ color: '#41FEB5' }}>Waiting hours reduced</span>
-                </div>
+              <div className="flex flex-col justify-center mt-[-7px] py-24 items-center bg-gradient-to-l from-green-700 to-black rounded-2xl px-14 lg:mb-12">
+                  <span><CloudIcon className="h-[100px] w-[100px] text-white" /></span>
+                  <span className="text-lg text-center text-white">Go live with web3 functionality</span>
+                {/* </div> */}
               </div>
             </div>
        
