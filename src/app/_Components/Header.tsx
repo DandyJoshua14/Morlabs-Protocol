56bece01e4df1d38c74d00a9c47fb817cf03485c
alivexem <athkinstestimony@gmail.com>
2025-01-20 14:59:16 +0100

    Style: Landing page completed and responsiveness added

---
+++ src/app/_Components/Header.tsx
@@ -0,0 +1,42 @@
+'use client'
+import { useState } from "react";
+import Image from "next/image";
+import { FaBars } from "react-icons/fa6";
+
+export default function Header({ onMenuClick }: { onMenuClick: () => void }) {
+    const [showMenu, setShowMenu] = useState(false);
+
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
+    };
+
+    return (
+        <>
+            <nav className="bg-black md:flex px-10 sm:flex-row hidden py-5 justify-between items-center">
+                <div>
+                    <div className="items-start">
+                        <Image src="/morlabs-logo.png" alt="morlabs_logo" width={150} height={150} />
+                    </div>
+                </div>
+                <div>
+                    <ul className="text-white flex justify-between gap-x-8">
+                        <li className="cursor-pointer hover:bg-green-700 p-2 rounded-lg">About</li>
+                        <li className="cursor-pointer hover:bg-green-700 p-2 rounded-lg">Features</li>
+                        <li className="cursor-pointer hover:bg-green-700 p-2 rounded-lg ">How it Works</li>
+                        <li className="cursor-pointer hover:bg-green-700 p-2 rounded-lg">Community</li>
+                        <li className="cursor-pointer hover:bg-green-700 p-2 rounded-lg">Whitepaper</li>
+                    </ul>
+                </div>
+            </nav>
+
+            <nav className="flex justify-between w-full items-center md:hidden bg-black text-white mt-[30px] p-4">
+                <div className="items-start">
+                    <Image src="/morlabs-logo.png" alt="morlabs_logo" width={150} height={150} />
+                </div>
+                <div className="cursor-pointer text-xl" onClick={onMenuClick}>
+                    <FaBars />
+                </div>
+            </nav>
+        </>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,93 +1,207 @@
 'use client'
-import { FaEthereum, FaBitcoin } from 'react-icons/fa';
-import { SiSolana, SiPolkadot, SiChainlink } from 'react-icons/si';
-import { SolanaIcon } from "./_Components/Icons/Solana"
-import { MagnifyingGlassIcon, ArrowPathIcon, CloudArrowUpIcon } from "@heroicons/react/24/outline";
-// sections
-import VideoIntro from "./_Components/get-started";
-import Feature from "./_Components/feature";
-import Testimonials from "./_Components/testimonials";
-import FAQ from "./_Components/faq";
-import Hero from "./_Components/hero";
-import { useState } from "react";
-// import Faqs from "./faqs";
-// export default function Campaign() {
+import { useState } from "react";;
+import Powered from "./_Components/powered";
+import { FaCircleArrowLeft, FaCircleArrowRight, FaCirclePlus} from "react-icons/fa6";
+import { IoClose } from "react-icons/io5";
+import Link from 'next/link'
+import { GrSearch } from "react-icons/gr";
+import { FiRefreshCw } from "react-icons/fi";
+import { LuCloudUpload } from "react-icons/lu";
+import Header from './_Components/Header';
+import Footer from './_Components/Footer';
+import Image from 'next/image';
+import { motion, AnimatePresence } from "framer-motion";
+
 export default function Home() {
-  const providers = [
-    { name: 'Ethereum', icon: <FaEthereum />, link: '#ethereum' },
-    { name: 'Solana', icon: <SiSolana />, link: '#solana' },
-    { name: 'Polkadot', icon: <SiPolkadot />, link: '#polkadot' },
-    { name: 'Chainlink', icon: <SiChainlink />, link: '#chainlink' },
-    { name: 'Bitcoin', icon: <FaBitcoin />, link: '#bitcoin' },
-  ];
+  const [openIndex, setOpenIndex] = useState<number | null>(null);
   const [showMenu, setShowMenu] = useState(false);
+
+  const toggleDropdown = (index: number) => {
+    if (openIndex === index) {
+      setOpenIndex(null);
+    } else {
+      setOpenIndex(index);
+    }
+  };
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+    // Prevent scrolling when menu is open
+    if (!showMenu) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'unset';
+    }
+  };
+
   return (
-    <div className='bg-black text-white'>
-      <div className="sm:h-screen">
-        {/* Navbar Section */}
-        <nav className="bg-black sm:flex px-10 sm:flex-row hidden py-5 justify-between items-center">
-          <div className="items-start"><img src="./morlabs-logo.png" alt="morlabs_logo" width={150} height={150} /></div>
-          <div>
-            <ul className="sm:flex text-white list-none justify-between gap-x-8">
-              <li>About</li>
-              <li>Features</li>
-              <li>How it Works</li>
-              <li>Community</li>
-              <li>Whitepaper</li>
+    <div className='bg-black min-h-[100vh] text-white pt-5'>
+      <Header onMenuClick={toggleMenu} />
+      
+      <AnimatePresence>
+        {showMenu && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className="fixed inset-0 bg-black z-50 flex items-center justify-center"
+          >
+            <div className="absolute top-8 mt-[40px] right-4 cursor-pointer text-xl" onClick={toggleMenu}>
+              <IoClose />
+            </div>
+            <ul className="text-white list-none flex flex-col items-center gap-y-8 text-xl">
+              <li className="cursor-pointer hover:bg-green-700 p-2 rounded-lg ">About</li>
+              <li className="cursor-pointer hover:bg-green-700 p-2 rounded-lg">Features</li>
+              <li className="cursor-pointer hover:bg-green-700 p-2 rounded-lg">How it Works</li>
+              <li className="cursor-pointer hover:bg-green-700 p-2 rounded-lg">Community</li>
+              <li className="cursor-pointer hover:bg-green-700 p-2 rounded-lg">Whitepaper</li>
             </ul>
-          </div>
-        </nav>
-        <nav className="sm:hidden block bg-black">
-          <div className="icon" onClick={() => { setShowMenu(!showMenu) }}>
-            <i className="fa fa-bars"></i>
-          </div>
-          {showMenu && <div> <ul className="text-white list-none gap-y-8">
-            <li>About</li>
-            <li>Features</li>
-            <li>How it Works</li>
-            <li>Community</li>
-            <li>Whitepaper</li>
-          </ul></div>}
-        </nav>
-        <div className="mt-10 max-w-5xl mx-auto">
-          <div className="px-5 text-6xl font-bold font-bricolage text-center m-auto">
-            <h3 className='py-3'>One Platform,</h3>
-            <h3 className=''>All the tools you need to build in Web3</h3>
-            <p className='p-2 font-normal text-lg whitespace-wrap'>Access ready-to-use APIs, SDKs, and AI-powered recommendations for simplified web3 development</p>
-            <button className="rounded-full bg-green-500 text-lg px-5 py-4">Join Waitlist</button>
-          </div>
-        </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
 
-        <div className="mt-10">
-          <div className="py-5 text-4xl font-bold font-bricolage text-center items-center">
-            <p className='p-2 font-normal text-xs whitespace-wrap'>Backed By:</p>
-            <div className=" text-lg px-5 justify-center items-center flex">
-              <button className="flex cursor-not-allowed p-5 rounded-full items-center"><SolanaIcon /> &nbsp; Solana
-              </button>
-            </div>
-          </div>
+      <div className="mt-10 max-w-5xl mx-auto">
+        <div className="px-2 text-[3.4rem] font-bold font-bricolage text-center m-auto">
+          <h3 className='py-3'>One Platform,</h3>
+          <h3 className="lg:whitespace-normal whitespace-wrap w-[90%]">All the tools you need to build in Web3</h3>
+          <p className='p-2 font-normal text-lg whitespace-wrap mt-[24px]'>Access ready-to-use APIs, SDKs, and AI-powered recommendations <br /> for simplified web3 development</p>
+          <Link href='/welcome'><button className="rounded-full h-[56px] hover:bg-green-700 box-border bg-green-500 text-lg px-5 py-4 mt-[24px]">Join Waitlist</button></Link>
         </div>
       </div>
-      <hr />
-      {/* About Section */}
-      <div>
-        <div className="mt-10 max-w-6xl mx-auto min-h-screen">
-          <div className="px-5 text-6xl font-bold font-bricolage text-center m-auto">
-            <h3 className='py-3'>Building in web3<br />shouldn't be rocket science</h3>
 
-            <p className='p-2 font-normal text-lg whitespace-wrap'>Developers waste hours hunting for the right tools, struggling with integrations, and navigating the steep learning curve of web3 development. The result? Slow progress, frustration, and missed opportunities</p>
-            <div className="sm:flex">
-              <div className="max-w-1xl w-1/6"></div>
-              <div className="rounded-xl max-w-4xl w-4/6 text-center text-black mx-auto mt-5 h-[500px] bg-white text-lg px-5 py-4"><h3 className="text-3xl py-6">
-                Leave Rocket Science to NASA.<br /> We've got you covered
-              </h3>
-                <p className='p-2 font-normal text-lg whitespace-wrap'>Morlabs Protocol simplifies Web3 development with high performance</p>
+      <Powered />
+
+      <div className='flex flex-col items-center mt-[140px] justify-center space-y-5 w-[100%] bg-black'>
+        <div className='border-y border-white h-[150px] flex items-center justify-center rounded-lg w-[90%]'>
+          <p className='text-white font-bold text-center text-[2.5rem]'>Learn</p>
+        </div>
+        <div className='border-y border-white h-[150px] flex items-center rounded-lg justify-center w-[90%]'>
+          <p className='text-white font-bold text-center text-[2.5rem]'>Test</p>
+        </div>
+        <div className='border-y border-white h-[150px] flex items-center rounded-lg justify-center w-[90%]'>
+          <p className='text-white font-bold text-center text-[2.5rem]'>Integrate</p>
+        </div>
+        <div className='border-y border-white h-[150px] flex items-center rounded-lg justify-center w-[90%]'>
+          <p className='text-white font-bold text-center text-[2.5rem]'>Deploy</p>
+        </div>
+      </div>
+
+      <div className='mt-[140px] flex tex-white flex-col items-center justify-center'>
+        <p className='text-center text-[2.4rem] my-[40px] font-bold'>Why developers need Morlabs</p>
+
+        <div className='flex items-center justify-center m-2 space-x-5'>
+          <FaCircleArrowLeft className='text-white text-[3rem]' />
+          <div className='h-[360px] text-white md:w-[600px] w-[95%] rounded-xl flex flex-col justify-center items-center' style={{ background: 'linear-gradient(90deg, #D0E6F5 0%, #FEAFD8 30%, #C482FA 60%)' }}>
+            <Image alt='hourglass' src='/holographic hourglass.png' width={73} height={120} />
+            <p className='text-center my-5'>Save Time</p>
+            <p className='text-center'>Stop reinventing the wheel. <br /> Access pre-built APIs and SDKs to get your projects up <br /> and running in no time.</p>
+          </div>
+          <FaCircleArrowRight className='text-white text-[3rem]' />
+        </div>
+      </div>
+
+      <div className='flex justify-center items-center'>
+        <div className='h-auto w-[95%] border-white border-[1px] flex flex-col md:flex-row items-center justify-evenly rounded-xl mt-[80px] p-[40px]'>
+          <div className='w-[70%] h-[100%] flex flex-col'>
+            <p className='text-[2.2rem] font-bold'>Build smarter, no matter your experience level</p>
+            <p className='text-[1.2rem]'>MorLabs empowers every developer to innovate faster and more efficiently in Web3.</p>
+
+            <div className='flex space-y-4 flex-col md:flex-row items-center gap-x-6 mt-[40px]'>
+              <div className='md:w-[450px] w-[85vw] box-border lg:h-[250px] md:h-[350px] h-auto text-white border-white border-[0.5px] rounded-xl p-4 flex-start items-center'>
+                <Image src='/MedalMilitary.png' alt='medal' height={40} width={40} />
+                <p className='text-[1.3rem] mt-4 font-bold'>For experienced devs</p>
+                <p className='text-[1rem] mt-[40px]'>Streamline your workflow with ready-to-use tools that reduce time spent on repetitive tasks.</p>
               </div>
-              <div className="w-1/6"></div>
+
+              <div className='md:w-[450px] w-[85vw] lg:h-[250px] md:h-[350px] box-border h-auto text-white border-white border-[0.5px] rounded-xl p-4 flex-start items-center'>
+                <Image src='/Medal.png' alt='medal' height={40} width={40} />
+                <p className='text-[1.3rem] mt-4 font-bold'>For new devs</p>
+                <p className='text-[1rem] mt-[40px]'>Skip the steep learning curve. Build with confidence using tools that simplify complex processes.</p>
+              </div>
             </div>
           </div>
+          <Image className="mt-7 md:mt-0" src='/crisscross form.png' alt='crisscross form' width={200} height={200} />
         </div>
       </div>
+
+      <div className='flex-start mt-[100px]'>
+        <p className='text-left text-white font-bold my-7 md:ml-14 ml-7 text-[1.7rem]'>How It Works</p>
+
+      </div>
+      <div className='flex items-center flex-col md:flex-row space-y-6 justify-evenly gap-x-3 mt-[40px]'>
+        <div className='h-[300px] w-[370px] justify-center flex-col gap-y-10 items-center flex rounded-xl' style={{ background: 'linear-gradient(90deg, #000000 0%, #1AB479 90%)' }}>
+          <GrSearch className='text-white text-[3rem]' />
+          <p className='text-white text-center text-[1.5rem] font-bold'>Browse SDKs & APIs</p>
+        </div>
+        <div className='h-[300px] w-[370px] justify-center flex-col gap-y-10 items-center flex rounded-xl' style={{ background: 'linear-gradient(90deg, #000000 0%, #1AB479 90%)' }}>
+          <FiRefreshCw className='text-white text-[3rem]' />
+          <p className='text-white text-center text-[1.5rem] font-bold'>Integrate seamlessly into your project</p>
+        </div>
+        <div className='h-[300px] w-[370px] justify-center flex-col gap-y-10 items-center flex rounded-xl' style={{ background: 'linear-gradient(90deg, #000000 0%, #1AB479 90%)' }}>
+          <LuCloudUpload className='text-white text-[3rem]' />
+          <p className='text-white text-center text-[1.5rem] font-bold'>Go live with web3 functionality</p>
+        </div>
+      </div>
+
+      <div className='flex items-center flex-col justify-center mt-[100px] relative'>
+        <p className='text-white font-bold text-[2.6rem] text-center'>The future of Web3 development  is waiting. <br /> Are you in?</p>
+        <Link href='/welcome'><button className="rounded-full h-[56px] box-border bg-green-500 hover:bg-green-700 text-lg px-5 py-4 mt-[24px]">LFG! Join Waitlist</button></Link>
+        {/* <div className='w-[35%] h-[50%] absolute' style={{ background: 'radial-gradient(circle at center, #25FEAA 0%, #20DB93 40%, #169866 60%, rgba(0,0,0,1) 100%)' }}></div> */}
+      </div>
+
+
+      <p className='text-white font-bold text-[2rem] text-center mt-[200px]'>Morlabs 101: What you need to know</p>
+      <div className='flex flex-col space-y-4 mb-[170px] justify-center items-center mt-[30px]'>
+        <div className='md:w-[800px] w-[95%] flex flex-col'>
+          <div className='h-[70px] box-border flex items-center justify-between p-3 text-white border-white rounded-lg border-[0.5px]'>
+            <p className='text-[1.2rem]'>What is Morlabs?</p>
+            <FaCirclePlus className='text-[1.8rem] cursor-pointer hover:text-green-500' onClick={() => toggleDropdown(0)} />
+          </div>
+          {openIndex === 0 && (
+            <div className='p-5 text-white'>
+             <p> Morlabs is an open-source platform that provides developers with a suite of tools and services to build and deploy Web3 applications.</p>
+             </div>
+          )}
+        </div>
+
+        <div className='md:w-[800px] w-[95%] flex flex-col'>
+          <div className='h-[70px] box-border flex items-center justify-between p-3 text-white border-white rounded-lg border-[0.5px]'>
+            <p className='text-[1.2rem]'>Who is it for?</p>
+            <FaCirclePlus className='text-[1.8rem] cursor-pointer hover:text-green-500' onClick={() => toggleDropdown(1)} />
+          </div>
+          {openIndex === 1 && (
+            <div className='p-5 text-white'>
+              <p>Morlabs is for developers who want to build Web3 applications faster and more efficiently.</p>
+            </div>
+          )}
+        </div>
+
+        <div className='md:w-[800px] w-[95%] flex flex-col'>
+          <div className='h-[70px] box-border flex items-center justify-between p-3 text-white border-white rounded-lg border-[0.5px]'>
+            <p className='text-[1.2rem]'>What makes Morlabs different?</p>
+            <FaCirclePlus className='text-[1.8rem] cursor-pointer hover:text-green-500' onClick={() => toggleDropdown(2)} />
+          </div>
+          {openIndex === 2 && (
+           <div className='p-5 text-white'>
+              <p>Morlabs is different because it provides a suite of tools and services that are easy to use and integrate into your project.</p>
+            </div>
+          )}
+        </div>
+
+        <div className='md:w-[800px] w-[95%] flex flex-col'>
+          <div className='h-[70px] box-border flex items-center justify-between p-3 text-white border-white rounded-lg border-[0.5px]'>
+            <p className='text-[1.2rem]'>When will the platform be available?</p>
+            <FaCirclePlus className='text-[1.8rem] cursor-pointer hover:text-green-500' onClick={() => toggleDropdown(3)} />
+          </div>
+          {openIndex === 3 && (
+            <div className='p-5 text-white'>
+              <p>Morlabs is currently in development and will be available in the future.</p>
+            </div>
+          )}
+        </div>
+      </div>
+
+      <Footer />
     </div>
   );
 }
