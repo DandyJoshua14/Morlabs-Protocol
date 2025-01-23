1708d7e6e3b59dbea963189ad134c2f221ddbb74
DandyJoshua14 <dandyjoshua14@gmail.com>
2025-01-23 11:42:21 +0100

    preview: metadata for SEO

---
+++ src/app/_Components/metadatafunc.ts
@@ -0,0 +1,6 @@
+export const metaData = {
+    metadataBase: new URL("https://morlabsprotocol.com"),
+    keywords: ["morlabs", "morlabs protocol", "api and sdk", "best api and sdk", "transition from web2 to web3"],
+    title: { default: "Morlabs Protocol", template: `%s | Morlabs Protocol` },
+    openGraph: { description: "An API & SDK directory for developers" },
+};
